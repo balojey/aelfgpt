90fb4386ccaee6aa32ffbeed3c242b850efe27e7
Ken Harlbar <balotech514@gmail.com>
2024-07-16 23:18:37 +0000

    Hot fix

---
+++ resources/utils/rag_populate_db.py
@@ -0,0 +1,101 @@
+import os, torch, sys, logging
+from dotenv import find_dotenv, dotenv_values
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+# from llama_index.vector_stores.mongodb import MongoDBAtlasVectorSearch
+from llama_index.core import StorageContext
+# from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+from llama_index.readers.json import JSONReader
+from llama_index.core import Settings
+from llama_index.vector_stores.chroma import ChromaVectorStore
+import chromadb
+from llama_index.llms.ollama import Ollama
+
+
+# Setup logging. To see more logging, set the level to DEBUG
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+""" Load Settings """
+
+# Change system path to root direcotry
+sys.path.insert(0, '../')
+
+# _ = load_dotenv(find_dotenv()) # read local .env file
+config = dotenv_values(find_dotenv())
+
+# create client and a new collection
+# chroma_client = chromadb.EphemeralClient()
+# chroma_collection = chroma_client.create_collection("quickstart")
+db = chromadb.PersistentClient(path="./chroma_db")
+chroma_collection = db.get_or_create_collection("quickstart")
+
+
+# ATLAS_URI = config.get('ATLAS_URI')
+
+# if not ATLAS_URI:
+#     raise Exception ("'ATLAS_URI' is not set.  Please set it above to continue...")
+# else:
+#     print("ATLAS_URI Connection string found:", ATLAS_URI)
+
+# # Define DB variables
+# DB_NAME = 'ava'
+# COLLECTION_NAME = 'sui'
+# INDEX_NAME = 'idx_embedding'
+
+# LlamaIndex will download embeddings models as needed
+# Set llamaindex cache dir to ../cache dir here (Default is system tmp)
+# This way, we can easily see downloaded artifacts
+os.environ['LLAMA_INDEX_CACHE_DIR'] = os.path.join(os.path.abspath('../'), 'cache')
+
+# mongodb_client = pymongo.MongoClient(ATLAS_URI)
+
+""" Clear out collection """
+
+# database = mongodb_client[DB_NAME]
+# collection = database [COLLECTION_NAME]
+
+# doc_count = collection.count_documents (filter = {})
+# print (f"Document count before delete : {doc_count:,}")
+
+# result = collection.delete_many(filter= {})
+# print (f"Deleted docs : {result.deleted_count}")
+
+""" Setup Embeddings """
+
+llm = Ollama(model="codeqwen", request_timeout=60.0, base_url="http://ollama:11434")
+Settings.llm = llm
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+Settings.embed_model = embed_model
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+""" Read PDF Documents """
+
+# pdf_data_dir = './ava/ava_backend/resources/pdfs'
+# pdf_docs = SimpleDirectoryReader(
+#         input_dir=pdf_data_dir
+# ).load_data()
+
+# print (f"Loaded {len(pdf_docs)} chunks from '{pdf_data_dir}'")
+
+""" Read JSON Documents """
+
+json_data_file = './resources/aelfdocs.json'
+json_docs = JSONReader(levels_back=0).load_data(json_data_file)
+
+print (f"Loaded {len(json_docs)} chunks from '{json_data_file}'")
+
+""" Index the Documents and Store Them Into MongoDB Atlas """
+
+# pdf_index = VectorStoreIndex.from_documents(
+#     pdf_docs, 
+#     storage_context=storage_context,
+#     service_context=service_context,
+# )
+
+json_index = VectorStoreIndex.from_documents(
+    json_docs,
+    storage_context=storage_context,
+    embed_model=embed_model
+)
--- src/app.py
+++ src/app.py
@@ -1,28 +1,76 @@
 import chainlit as cl
 from llama_index.llms.ollama import Ollama
 from llama_index.core.llms import ChatMessage, ChatResponse, ChatResponseAsyncGen
+from llama_index.core.memory import ChatMemoryBuffer
+import os, torch, sys, logging
+from dotenv import find_dotenv, dotenv_values
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex
+from llama_index.readers.json import JSONReader
+from llama_index.core import Settings
+from llama_index.vector_stores.chroma import ChromaVectorStore
+import chromadb
 
+
+# Setup logging. To see more logging, set the level to DEBUG
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+""" Load Settings """
+
+# Change system path to root direcotry
+sys.path.insert(0, '../')
+
+# _ = load_dotenv(find_dotenv()) # read local .env file
+config = dotenv_values(find_dotenv())
+
+# create client and a new collection
+db = chromadb.PersistentClient(path="./chroma_db")
+chroma_collection = db.get_collection("./chroma_db")
+
+# LlamaIndex will download embeddings models as needed
+# Set llamaindex cache dir to ../cache dir here (Default is system tmp)
+# This way, we can easily see downloaded artifacts
+os.environ['LLAMA_INDEX_CACHE_DIR'] = os.path.join(os.path.abspath('../'), 'cache')
+
+""" Setup Embedding Model """
+
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+Settings.embed_model = embed_model
+
+""" Setup vector """
 
 llm = Ollama(model="codeqwen", request_timeout=60.0, base_url="http://ollama:11434")
-""
+Settings.llm = llm
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+index = VectorStoreIndex.from_vector_store(
+    vector_store=vector_store
+)
+
+memory = ChatMemoryBuffer.from_defaults()
+chat_engine = index.as_chat_engine(
+    chat_mode="context",
+    memory=memory,
+    system_prompt=(
+        """
+            You're a RAG-Enabled LLM for the aelf blockchain documentation, \
+            a smart contract debugger on the aelf blockchain, \
+            and a natural language smart contract generator for the aelf blockchain.
+            Your name is AelfGPT.
+        """
+    )
+)
+
 
 @cl.on_message
 async def main(message: cl.Message):
-    messages = [
-            ChatMessage(
-                role="system", content="""
-                    You're a RAG-Enabled LLM for the aelf blockchain documentation, \
-                    a smart contract debugger on the aelf blockchain, \
-                    and a natural language smart contract generator for the aelf blockchain.
-                    Your name is AelfGPT.
-                """
-            ),
-            ChatMessage(role="user", content=message.content),
-        ]
-    resp: ChatResponseAsyncGen = await llm.achat(messages=messages)
+    response = await chat_engine.achat(message=message.content)
+    response.is_dummy_stream = True
     msg = cl.Message(content="", author="AelfGPT")
 
     # Send a response back to the user
-    for token in resp.message.content:
+    for token in response.response_gen:
         await msg.stream_token(token=token)
     await msg.send()
