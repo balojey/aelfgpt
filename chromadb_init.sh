b93a086c77921750f17e40982b4842a5d7386618
ademolabalogun91 <ademolabalogun91@gmail.com>
2024-07-17 23:54:33 +0000

    Quick fix

--- chromadb_init.sh
+++ chromadb_init.sh
@@ -1,2 +1,6 @@
-git clone https://github.com/chroma-core/chroma.git
-docker-compose -f chromadb/docker-compose.yml up -d --build
+cd chromadb
+if [ ! -d "chroma" ]; then
+  git clone https://github.com/chroma-core/chroma.git
+fi
+cd -
+docker compose -f chromadb/docker-compose.yml up -d --build
--- ollama_init.sh
+++ ollama_init.sh
@@ -1,1 +1,18 @@
-docker-compose -f ollama/docker-compose.yml up -d --build
+# mkdir -p /usr/share/keyrings
+# touch /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+
+# curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+#   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+#     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+#     tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# apt-get update
+# apt-get install -y nvidia-container-toolkit
+
+# # Configure NVIDIA Container Toolkit
+# nvidia-ctk runtime configure --runtime=docker
+# systemctl restart docker
+
+# Test GPU integration
+# docker run --gpus all nvidia/cuda:11.5.2-base-ubuntu20.04 nvidia-smi
+
+docker compose -f ollama/docker-compose.yml up -d --build
