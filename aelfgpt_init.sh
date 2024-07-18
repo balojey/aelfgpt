751cf12abf1d821173a716ac6ffc846f2dfb4590
balo <98645847+balojey@users.noreply.github.com>
2024-07-18 15:49:47 +0000

    Changed poetry python version to 3.10

--- aelfgpt_init.sh
+++ aelfgpt_init.sh
@@ -6,6 +6,17 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+# Change to the aelfgpt directory
+cd aelfgpt
+
+# Check if the chroma directory exists, if not, clone it
+if [ ! -f ".env" ]; then
+  cp example.env .env
+fi
+
+# Change back to the previous directory
+cd -
+
 # Run the appropriate docker compose command based on the parameter
 if [ "$1" == "up" ]; then
   docker compose -f aelfgpt/docker-compose.yml up -d --build
