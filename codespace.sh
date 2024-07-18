3f421034d19c509116e14a0e684d22712d5494c1
balo <98645847+balojey@users.noreply.github.com>
2024-07-18 18:13:41 +0000

    Hot fix

---
+++ codespace.sh
@@ -0,0 +1,8 @@
+./chromadb_init.sh up
+./ollama_init.sh up
+/bye
+cd aelfgpt
+cp example.env .env
+pip install -r requirements.txt
+python resources/utils/rag_populate_db.py
+chainlit run src/app.py -hw
