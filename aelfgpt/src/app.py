33892cccd99f22feb16bf63ec1eb53b5a4ee999e
Ademola Balogun <ademolabalogun91@gmail.com>
2024-07-27 02:43:27 +0000

    Hot fix

--- aelfgpt/src/app.py
+++ aelfgpt/src/app.py
@@ -7,7 +7,7 @@ from dotenv import find_dotenv, dotenv_values
 
 # Import chainlit and llama_index
 import chainlit as cl
-from llama_index.llms.ollama import Ollama
+from llama_index.llms.gemini import Gemini
 from llama_index.core.llms import ChatMessage, ChatResponse, ChatResponseAsyncGen
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
@@ -20,11 +20,12 @@ from llama_index.core.indices.base import BaseChatEngine
 # Import langchain
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_community.agent_toolkits import create_sql_agent
-from langchain_community.chat_models import ChatOllama as SQLChatModel
+from langchain_google_genai import GoogleGenerativeAI as SQLChatModel
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
+from langchain.agents.agent import AgentExecutor
 
 
 # Setup logging
@@ -39,9 +40,10 @@ llm_url = config.get("AELFGPT_LLM_URL")
 llm_name = config.get("AELFGPT_LLM_NAME")
 atlas_uri = config.get("ATLAS_URI")
 db_url = config.get("DB_URL")
+gemini_api_key = config.get("GEMINI_API_KEY")
 
-if not all([llm_url, atlas_uri, llm_name, db_url]):
-    logging.error("Environment variables AELFGPT_LLM_URL, AELFGPT_LLM_NAME, DB_URL, or ATLAS_URI are not set.")
+if not all([llm_url, atlas_uri, llm_name, db_url, gemini_api_key]):
+    logging.error("Environment variables AELFGPT_LLM_URL, AELFGPT_LLM_NAME, DB_URL, GEMINI_API_KEY, or ATLAS_URI are not set.")
 
 # LlamaIndex will download embeddings models as needed
 # Set llamaindex cache dir to ../cache dir here (Default is system tmp)
@@ -91,10 +93,16 @@ async def chat_profile():
 
 @cl.on_chat_start
 async def start():
-    llm = Ollama(model=llm_name, request_timeout=120.0, base_url=llm_url)
+    llm = Gemini(api_key=gemini_api_key, model="models/gemini-1.5-flash-latest")
     Settings.llm = llm
     Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
     Settings.context_window = 4096
+
+    vector_store = MongoDBAtlasVectorSearch(mongodb_client = mongodb_client,
+                    db_name = DB_NAME, collection_name = COLLECTION_NAME,
+                    index_name  = 'idx_embedding',
+                )
+    index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
     Settings.callback_manager = CallbackManager([cl.LlamaIndexCallbackHandler()])
     thorin = index.as_chat_engine(
@@ -110,7 +118,7 @@ async def start():
         memory=ChatMemoryBuffer.from_defaults()
     )
 
-    llm = SQLChatModel(model=llm_name, request_timeout=120.0)
+    llm = SQLChatModel(model="gemini-1.5-flash-latest", google_api_key=gemini_api_key)
     gandalf = create_sql_agent(
         llm, db=db, agent_type="openai-tools", verbose=True,
         callback_manager=[cl.LangchainCallbackHandler()]
@@ -139,9 +147,12 @@ async def main(message: cl.Message):
             await msg.stream_token(token)
         await msg.send()
     else:
-        gandalf = cl.user_session.get("gandalf")
+        gandalf: AgentExecutor = cl.user_session.get("gandalf")
         msg = cl.Message(content="")
 
-        for chunk in gandalf.invoke(message.content)["output"]:
-            await msg.stream_token(chunk)
-        await msg.send()
+        # for chunk in gandalf.invoke(message.content)["output"]:
+        #     await msg.stream_token(chunk)
+        # await msg.send()
+        res = gandalf.invoke(message.content)
+        print(f"\n\nResponse: {res}\n\n")
+        msg.send(res)
